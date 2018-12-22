e20353bed265e5abad612ccc42e5aa6df3121c0c
Rukmini Ananthanarayanan <46060825+rukminihere@users.noreply.github.com>
2018-12-22 12:37:59 -0600

    Add files via upload
    Updates to Select Person. Added Wildcard in search filter

--- SQL Scripts/Stored Procedures/usp_SelectPerson.sql
+++ SQL Scripts/Stored Procedures/usp_SelectPerson.sql
@@ -9,11 +9,11 @@ GO
 
 CREATE PROCEDURE uspSelectPerson
 (
-    @FirstName Varchar(50)=null,
-    @LastName Varchar(50)=null,
+    @FirstName Varchar(50) = NULL,
+    @LastName Varchar(50) = NULL,
     @StateID INT = NULL,
-    @Gender char(1)=null,
-    @DOB DATETIME =null   
+    @Gender char(1) = NULL,
+    @DOB DATETIME = NULL   
 )
 AS
 SET NOCOUNT ON;
@@ -27,10 +27,10 @@ BEGIN
 	FROM 
 		Person	
 	WHERE 
-		COALESCE(First_Name,'')=COALESCE(@FirstName,First_Name,'')
-		AND COALESCE(Last_Name,'')=COALESCE(@LastName,Last_Name,'')
-		AND COALESCE(State_Id,'')=COALESCE(@StateID,State_Id,'')
-		AND COALESCE(Gender,'')=COALESCE(@Gender,Gender,'')
-		AND DOB=COALESCE(@DOB,DOB,'')            
+		COALESCE(First_Name,'') LIKE ('%' + COALESCE(@FirstName,First_Name,'') + '%')
+		AND COALESCE(Last_Name,'') LIKE ('%' +COALESCE(@LastName,Last_Name,'') + '%')
+		AND COALESCE(State_Id,'') = COALESCE(@StateID,State_Id,'')
+		AND COALESCE(Gender,'') = COALESCE(@Gender,Gender,'')
+		AND COALESCE(DOB,'') = COALESCE(@DOB,DOB,'')          
 END
     
